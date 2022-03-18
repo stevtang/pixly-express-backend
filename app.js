60198b591f0d5a7e48e9e0be60f9a557ada245c0
stevtang <stevenmtang1@gmail.com>
2022-03-18 11:33:16 -0700

    re-ordered paths

--- app.js
+++ app.js
@@ -16,34 +16,36 @@ app.use(express.json());
 // process traditional form data => req.body
 app.use(express.urlencoded({ extended: true }));
 
-const upload = multer({dest:'./uploads/'});
+const upload = multer({ dest: "./uploads/" });
 
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/images", async (req, res) => {
+  const searchTerm = req.query.q;
+  console.log("REQ PARAMS:", req.query);
+  const images = await getImagesFromDb(searchTerm);
+  res.send(images);
 });
 
-app.get("/images", async (req,res)=>{
-  const searchTerm = req.query.q; 
-  console.log("REQ PARAMS:", req.query);
-  const images =  await getImagesFromDb(searchTerm);
-  res.send(images);
-})
-
-app.post("/images", upload.single('image'), async (req, res) => {
+app.post("/images", upload.single("image"), async (req, res) => {
   console.log("REQ.FILE", req.file);
 
-  console.log("REQ.BODY.IMAGE:",req.body);
+  console.log("REQ.BODY.IMAGE:", req.body);
   // console.log("REQ.FILE", req.file);
 
   const s3ImagePath = await imageFileHandler.saveToStorage(req.file);
   const exifMeta = await imageFileHandler.extractExif(req.file);
 
-    const dbFields = {filename: req.file.filename, s3ImagePath: s3ImagePath, ...exifMeta};
-    console.log("DBFIELDS:", dbFields);
-    const imageData = await imageFileHandler.saveImageMetadataToDb(dbFields);
+  const dbFields = {
+    filename: req.file.filename,
+    s3ImagePath: s3ImagePath,
+    ...exifMeta,
+  };
+  console.log("DBFIELDS:", dbFields);
+  const imageData = await imageFileHandler.saveImageMetadataToDb(dbFields);
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
